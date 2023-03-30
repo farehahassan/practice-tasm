2f9100aa123672a9d9bd354236b74a5ebbf5ad9c
Fareha <hfariha104@gmail.com>
2023-03-30 20:25:41 +0500

    add new

---
+++ Delet_08h.asm
@@ -0,0 +1,23 @@
+.model small
+.stack 100h
+.data
+.code
+main proc 
+mov ah , 2 
+mov dl ,"F"
+int 21h
+
+mov dl , 0Ah
+int 21h
+
+mov dl , "a"
+int 21h
+
+mov dl , 08h
+int 21h
+
+mov ah , 4ch 
+int 21h
+
+main endp
+end main
--- lab1.asm
+++ lab1.asm
@@ -5,17 +5,17 @@
 .Data
 .code
 main proc 
-mov dl, "a"
 mov ah, 2
+mov dl, "a"
+
 int 21h
 
 mov dl, "l"
-mov ah, 2
+
 int 21h
 
 mov dl, "i"
-mov ah, 2
-int 21h
 
+int 21h
  main endp
  end main
